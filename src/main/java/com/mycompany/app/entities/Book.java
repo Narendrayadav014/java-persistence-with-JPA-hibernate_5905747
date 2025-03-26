31785f8c11ca7fa3d686a6d64e14c8a0f00d7e5f
Narendrayadav014 <85392032+Narendrayadav014@users.noreply.github.com>
2025-03-26 08:24:11 +0000

    added table column fields

--- src/main/java/com/mycompany/app/entities/Book.java
+++ src/main/java/com/mycompany/app/entities/Book.java
@@ -1,5 +1,6 @@
 package com.mycompany.app.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
@@ -7,4 +8,33 @@ import jakarta.persistence.Table;
 @Table(name = "book")
 public class Book {
 
+  @Column(name = "book_id")
+  private int id;
+  @Column(name = "book_name")
+  private String name;
+  
+  private String isbn;
+
+  public int getId(){
+    return id;
+  }
+
+  public void setId(int id){
+    this.id =  id;
+  }
+  
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+  public String getIsbn(){
+    return isbn;
+  }
+
+  public void setIsbn(String isbn){
+    this.isbn =  isbn;
+  }
 }
